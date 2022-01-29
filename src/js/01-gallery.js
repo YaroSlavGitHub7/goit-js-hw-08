c2e6d9542a4239de7fdceb56bef4b4ae80c34f1a
YaroSlavGitHub7 <yatret74@gmail.com>
2022-01-30 00:12:34 +0200

    task8
    галерея

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,33 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// создание разметки
+// console.log(createGalleryItemsMarkup(galleryItems));
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return /*шаблон одного элемента*/ ` 
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>
+        `;
+    })
+        .join('');
+}
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
