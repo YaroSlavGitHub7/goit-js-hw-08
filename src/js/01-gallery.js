ab48b534c82673c3d8595843cca81091190a16ed
YaroSlavGitHub7 <yatret74@gmail.com>
2022-02-02 00:04:50 +0200

    task8
    разметка галереи

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+/* в package.json добавили parcel-plugin-handlebars */
+import imgGallaryTml from '../templates/imgGallery'; /* в templates поместили шаблон и заимпортировали его*/
+/*вызвали ф-ю шаблона, закинули в неё объект*/
+// console.log(imgGallaryTml(galleryItems[0])); /*пришла ф-ия с значением 1го элемента*/
+
+/*через ИЧ получаем массив*/
+import imgGallaryTmlArr from '../templates/imgGalleryArr'; /* в templates поместили шаблон и применили ИЧ, заимпортировали его*/
+
+
 
 const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// создание разметки
-// console.log(createGalleryItemsMarkup(galleryItems));
 
 function createGalleryItemsMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return /*шаблон одного элемента*/ ` 
-        <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-            />
-        </a>
-        </div>
-        `;
-    })
-        .join('');
+        // return galleryItems.map(galleryItem => imgGallaryTml(galleryItem)).join(''); /*мэпаем масив изображений -вызывается шаблон для каждого элемента*/
+    // return galleryItems.map(imgGallaryTml).join(''); /*тоже самое - короткая запись*/
+
+    return imgGallaryTmlArr(galleryItems); /*ещё короче запись*/
 }
+console.log(imgGallaryTmlArr(galleryItems));/*создана разметка*/
+
+
 // Описан в документации
 import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
